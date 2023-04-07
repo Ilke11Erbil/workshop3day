dc99fcd591d6175bfcf0ef7ad571e61de5406ad7
Ilke11Erbil <129994698+Ilke11Erbil@users.noreply.github.com>
2023-04-07 12:36:34 +0300

    Add files via upload

---
+++ workshop3.py
@@ -0,0 +1,12 @@
+numberList =[]
+for number in range(10):
+ number = input("Lütfen Sayı Giriniz:")
+ numberList.append(number)
+
+en_buyuk = max(numberList)
+print("Liste içindeki en büyük sayi:", en_buyuk )
+
+ 
+
+
+
