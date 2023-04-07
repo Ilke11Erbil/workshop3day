1c3e8aaea7cef5834516690bea1d622d299ee730
Ilke11Erbil <129994698+Ilke11Erbil@users.noreply.github.com>
2023-04-07 12:35:52 +0300

    Add files via upload

---
+++ cift.py
@@ -0,0 +1,14 @@
+numberList = []
+upperLimit = int(input("üst limit belirleyiniz:"))
+
+for ciftsayi in range(0,upperLimit):
+  if (int(ciftsayi) >=0 and (ciftsayi)%2 == 0):
+    numberList.append(numberList)
+    print ("ciftsayilar:", ciftsayi )
+lowerLimit = int(input("alt limit belirleyiniz:"))
+numberList2 = []
+for ciftsayi2 in range (lowerLimit,upperLimit):
+  if(int(ciftsayi2)%2 == 0):
+    numberList.append(numberList)
+    print("ciftsayilar:",ciftsayi2)
+
